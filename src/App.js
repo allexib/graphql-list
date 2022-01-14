ede6f24419781245020fcc0cd66ebaa2ae086f33
allexib <ooomegasib@gmail.com>
2022-01-14 13:58:55 +0300

    simple request,hasure-gql,get todos with secret

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react'
+import {useQuery} from '@apollo/react-hooks'
+import {gql} from 'apollo-boost'
+
+const GET_TODOS = gql`
+query getTodos {
+  todos {
+    done
+    id
+    text
+  }
+}
+`
+
 
 function App() {
+    const stuff = useQuery(GET_TODOS)
+console.log(stuff)
+
   return (
     <div >
       app
--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ApolloClient from 'apollo-boost'
+import {ApolloProvider} from '@apollo/react-hooks'
+
+const client = new ApolloClient({
+    uri: 'https://evolving-fowl-34.hasura.app/v1/graphql',
+    headers:{
+        'x-hasura-admin-secret':'MGUOinunS8hI5hWFwQ3fOMbQ5NNiDCAaarjlO6VC8ivH5UYCAuWUcmu20Qe9nME4'
+    }
+})
 
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App/>
+    </ApolloProvider>
+    , document.getElementById('root'));
+//MGUOinunS8hI5hWFwQ3fOMbQ5NNiDCAaarjlO6VC8ivH5UYCAuWUcmu20Qe9nME4
