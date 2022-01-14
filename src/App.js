939ce2bf74c425545fab9e5e58beb56fa333442c
allexib <ooomegasib@gmail.com>
2022-01-14 15:02:49 +0300

    styled tachyons

--- src/App.js
+++ src/App.js
@@ -20,24 +20,29 @@ function App() {
     if (error) return <div>Error fetching todos</div>
 
 
-    return <div>
-        <h1>graphql checklist</h1>
+    return <div className='vh-100 code flex flex-column items-center bg-purple white pa4 fl-1'>
+        <h1 className='f2-l'>graphql checklist {''}
+            <span role='img' aria-label='Checkmark'>🎂✔</span>
+        </h1>
         {/*Todo Form*/}
-        <form>
-            <input
-                type='text'
-                placeholder='write y todo'
+        <form className="mb3">
+            <input className='pa2 f4 b--dashed'
+                   type='text'
+                   placeholder='write y todo'
             />
-            <button type='submit'>create</button>
+            <button className='pa2 f4 bg-green' type='submit'>create</button>
         </form>
         {/*Todo List*/}
-        <div>
+        <div className='flex item-center justify-center flex-column'>
             {data.todos.map(todo => (
                 <p key={todo.id}>
-            <span>
+            <span className='pointer list pa1 f3'>
                 {todo.text}
             </span>
-                    <button>&times;</button>
+                    <button className='bg-transparent bn f4'>
+                        <span className='red'>&times;</span>
+
+                    </button>
                 </p>
             ))}
         </div>
